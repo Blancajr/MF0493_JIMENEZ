141d12156fa981419eade66eb94d38dbaf8a1b07
blanc <blanc@BLANCA.local>
2024-06-07 10:51:44 +0200

    COMMIT

--- src/ConexionBD.java
+++ src/ConexionBD.java
@@ -1,9 +1,73 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import conexion.ConexionBD;
+
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Clase que permite la conexion con la base de datos 
+ * registrando un driver de tipo MariaDB
+ */
 public class ConexionBD {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/**Propiedades de la conexión*/
+	//cambiar el dato para conectar a otra base de datos
+	private static String database="empresa";
+	private static String usuario="root";
+	private static String contrasena="";
+	private static String url="jdbc:mariadb://localhost/"+database;
+	
+	//Objeto Connection que debemos usar en JDBC
+	private Connection conexion=null;
+	
+	/**
+	 * Método de la clase que devuelve el objeto Connection 
+	 * necesario para operar con la base de datos
+	 * @return el objeto Connection
+	 */
+	public Connection getConexion() {
+		if (this.conexion!=null) {
+			//Ya está la conexión creada, la devuelvo
+			return this.conexion;
+		}
+		
+		try {
+			//Inicializamos la conexión a la base de datos
+			//Registrar el driver. Previamente habrá que haber añadido el driver al proyecto (Build Path)
+			Class.forName("org.mariadb.jdbc.Driver");
+			//Obtenemos el objeto Connection de la clase 
+			//DriverManager. Lanzará una excepción
+			//SQLException si no se puede conectar 
+			this.conexion = DriverManager.getConnection(url,usuario,contrasena);
+			System.out.println("Conexion correcta");
+		
+	} catch (ClassNotFoundException e) {
+		System.out.println("error al registrar el driver");
+		
+	} catch (SQLException e) {
+		System.out.println("No se puede conectar con la base de datos."+e.getLocalizedMessage());
+		
 	}
-
+		return this.conexion;
+}
+	
+	/**
+	 * Método de la clase que libera los recursos asociados a la conexion
+	 */
+	public void desconectar() {
+		if(this.conexion!=null) {
+			try {
+				this.conexion.close();
+			} catch (SQLException e) {
+				System.out.println("Error, no se puede liberar la conexión");
+			}
+		}
+	}
 }
--- src/Ppal/MostrarEditoriales.java
+++ src/Ppal/MostrarEditoriales.java
@@ -1,5 +1,57 @@
 package Ppal;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class MostrarEditoriales {
+	public class ConexionBD {
 
+		public static void main(String[] args) {
+			public class Departamentos {
+				public static void main(String[] args) {
+					ConexionBD conexion= new ConexionBD();
+					
+					System.out.println("Conectando a la base de datos...");
+					// Paso 1. Obtener la conexión
+					Connection con = conexion.getConexion();
+					
+					//Objetos necesarios para hacer una consulta
+							Statement sentencia= null;
+							ResultSet resultado=null;
+							
+					//Algún procesamiento con la base de datos..
+					// Paso 2. Obtener el Statement
+					try {
+						sentencia =con.createStatement();
+					
+					//Paso 3. Ejecutar la sentencia
+						resultado=sentencia.executeQuery("select * from departamentos"); 
+						System.out.println("Cod.Editorial\tNombre\tAnio");
+						
+						// Paso 4. Recorrer el resultado
+						while(resultado.next()) {
+							int codEditorial = resultado.getInt("codEditorial");
+							String nombre = resultado.getString("nombre");
+							int nombre = resultado.getInt("nombre");
+							
+							
+							System.out.println(codDepartamento+"\t"+codCentro+"\t"+codDirector+"\t"+tipo_dir+"\t"+presupuesto+"\t"+cod_dpto_jefe+"\t"+nombre);
+						}
+						
+					} catch (SQLException e) {
+						System.out.println("Error al liberar los recursos. "+e.getMessage());
+					}finally {			
+						
+					}
+					//Liberamos la conexión
+					System.out.println("Desconectando de la base de datos...");
+					conexion.desconectar();
+				}
+
+			}	
+
+		}
+
+	}
 }
